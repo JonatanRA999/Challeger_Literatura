efc41fe81b823889ac290f2e2116e548f31d36a8
JonatanR <jonatanjr-2017@outlook.es>
2024-07-14 06:38:02 -0500

    Proyecto literatura

---
+++ src/main/java/com/alura/challengerBiblioteca/ChallengerBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challengerBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challengerBiblioteca/ChallengerBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challengerBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
